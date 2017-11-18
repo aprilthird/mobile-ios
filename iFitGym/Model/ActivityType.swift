c16ff244dc65906ae2fb8f47be18a3404f7437fc
jrojas96 <jeffreyrm96@gmail.com>
2017-11-18 08:27:02 -0500

    First models

---
+++ iFitGym/Model/ActivityType.swift
@@ -0,0 +1,9 @@
+//
+//  ActivityType.swift
+//  iFitGym
+//
+//  Created by Luis Jeffrey Rojas Montes on 11/16/17.
+//  Copyright © 2017 Luis Jeffrey Rojas Montes. All rights reserved.
+//
+
+import Foundation
